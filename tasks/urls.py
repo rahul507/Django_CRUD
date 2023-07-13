256b23b33090de3d133f442ffd6d60f1348dd63c
gyan7 <gyansinghbudhiraja777@gmail.com>
2023-07-13 20:28:19 +0530

    updating

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,38 +1,3 @@
-# #-*- coding:utf-8 -*-
-
-# from django.urls import path, re_path
-# from . import views
-
-# # namespace
-# app_name = 'tasks'
-
-# urlpatterns = [
-#     # Create a task
-#     path('create/', views.TaskCreateView.as_view(), name='task_create'),
-#     # Retrieve task list
-#     # path('', views.TaskListView.as_view(), name='task_list'),
-#     path('', views.task_list, name='task_list'),
-#     # Retrieve single task object
-#     re_path(r'^(?P<pk>\d+)/$', views.TaskDetailView.as_view(), name='task_detail'),
-#     # Update a task
-#     re_path(r'^(?P<pk>\d+)/update/$', views.TaskUpdateView.as_view(), name='task_update'),
-#     # Delete a task
-#     re_path(r'^(?P<pk>\d+)/delete/$', views.TaskDeleteView.as_view(), name='task_delete')
-
-    # # Create a task
-    # path('create/', views.task_create, name='task_create'),
-    # # Retrieve task list
-    # path('', views.task_list, name='task_list'),
-    # # Retrieve single task object
-    # re_path(r'^(?P<pk>\d+)/$', views.task_detail, name='task_detail'),
-    # # Update a task
-    # re_path(r'^(?P<pk>\d+)/update/$', views.task_update, name='task_update'),
-    # # Delete a task
-    # re_path(r'^(?P<pk>\d+)/delete/$', views.task_delete, name='task_delete'),
-
-# ]
-# tasks/urls.py
-
 from django.urls import path, re_path
 from . import views
 
@@ -45,8 +10,8 @@ urlpatterns = [
     path('portal/', views.task_list, name='task_list'),
     path('home/', views.home, name='home'),
     path('', views.index, name='index'),
-    path('try1/', views.try1, name='try1'),
-    path('try2/', views.try2, name='try2'),
+    path('delete/', views.delete, name='delete'),
+    path('update/', views.update, name='update'),
     path('save/', views.save, name='save'),
     # Create a task
     path('create/', views.task_create, name='task_create'),
--- tasks/views.py
+++ tasks/views.py
@@ -1,82 +1,3 @@
-# from django.shortcuts import render, redirect, get_object_or_404
-# from django.urls import reverse, reverse_lazy
-# from .models import Task
-# from .forms import TaskForm
-
-# # Create your views here.
-# # Functional based view
-# # Create a task
-# def task_create(request):
-#     if request.method == "POST":
-#         form = TaskForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect(reverse("tasks:task_list"))
-#     else:
-#         form = TaskForm()
-
-#     return render(request, "tasks/task_form.html", { "form": form, })
-# #
-# #
-# # # Retrieve task list
-# def task_list(request):
-#     tasks = Task.objects.all()
-#     return render(request, "task_list.html", { "tasks": tasks,})
-# #
-# #
-# # # Retrieve a single task
-# def task_detail(request, pk):
-#     task = get_object_or_404(Task, pk=pk)
-#     return render(request, "tasks/task_detail.html", { "task": task, })
-# #
-# #
-# # # Update a single task
-# def task_update(request, pk):
-#     task_obj = get_object_or_404(Task, pk=pk)
-#     if request.method == 'POST':
-#         form = TaskForm(instance=task_obj, data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect(reverse("tasks:task_detail", args=[pk,]))
-#     else:
-#         form = TaskForm(instance=task_obj)
-
-#     return render(request, "tasks/task_form.html", { "form": form, "object": task_obj})
-# #
-# #
-# # # Delete a single task
-# def task_delete(request, pk):
-#     task_obj = get_object_or_404(Task, pk=pk)
-#     task_obj.delete()
-#     return redirect(reverse("tasks:task_list"))
-
-# # Class Based Views
-# from django.views.generic import ListView, DetailView, \
-#     CreateView, UpdateView, DeleteView
-
-# class TaskListView(ListView):
-#     model = Task
-#     context_object_name = 'tasks'
-
-# class TaskDetailView(DetailView):
-#     model = Task
-
-# class TaskCreateView(CreateView):
-#     model = Task
-#     form_class = TaskForm
-#     success_url = reverse_lazy('tasks:task_list')
-
-# class TaskUpdateView(UpdateView):
-#     model = Task
-#     form_class = TaskForm
-#     success_url = reverse_lazy('tasks:task_list')
-
-# class TaskDeleteView(DeleteView):
-#     model = Task
-#     success_url = reverse_lazy('tasks:task_list')
-
-
-# tasks/views.py
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
@@ -115,7 +36,7 @@ def save(request):
                 form = TaskForm(instance=task_obj)
 
     return render(request, "tasks/task_up.html", { "form": form, })
-def try1(request):
+def delete(request):
     if request.method == "POST":
         form = MyForm(request.POST)
         if form.is_valid():
@@ -141,7 +62,7 @@ def try1(request):
     return render(request, "delete.html", { "form": form, })
 
 
-def try2(request):
+def update(request):
     if request.method == "POST":
         form = MyForm(request.POST)
         if form.is_valid():
@@ -170,9 +91,13 @@ def try2(request):
 
 # Retrieve task list
 def task_list(request):
-    tasks = Task.objects.all()
-    print(tasks)
-    return render(request, "tasks/task_list.html", { "tasks": tasks,})
+    if request.user.is_authenticated:
+        tasks = Task.objects.all()
+        print(tasks)
+        return render(request, "tasks/task_list.html", { "tasks": tasks,})
+    else:
+         return render(request, "login.html")
+         
 def index(request):
     return render(request, "index.html")
 def home(request):
